23fd57801efd34136260c87bc1bc14c28980d8a8
kumarankit0 <ankitkr98999899@gmail.com>
2026-04-21 12:57:06 +0000

    Initial commit

---
+++ testing.py
@@ -0,0 +1,8 @@
+# git status 
+# git add testing.py
+# git commit -m "Add testing script"
+# git push origin main
+# git restore testing.py
+# git checkout -- testing.py
+# git rm testing.py
+# git commit -m "Remove testing script"
